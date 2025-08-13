22eff7d26ef176b2519fa94e90bc90ef429ca900
salahvastu <your-email@example.com>
2025-08-13 21:55:05 +0530

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
+  base: '/free-lms-router/',
   plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
-  },
 })
